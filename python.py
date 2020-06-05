ecd7bd2a4993b9862e7bcb93197f60ffeb6275e4
leo <leodenaro8@gmail.com>
2020-06-05 02:57:25 +0200

    hello world & multiline & newline

--- python.py
+++ python.py
@@ -1,2 +1,6 @@
-print('hello world')
-print(123)
+#this is a hello world appliciton
+print('hello world!\n')
+# multieline
+print("hello world2")
+print("""this is multeline hello world
+to day is a sunny""")
